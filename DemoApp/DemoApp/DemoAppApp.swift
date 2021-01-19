3f3778251b87d9fbb8176d18b309c15a5bf6c6b0
Felipe Espinoza <fespinozacast@gmail.com>
2021-01-19 14:44:51 +0100

    Add initial code for DemoApp

---
+++ DemoApp/DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Felipe Espinoza on 19/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
