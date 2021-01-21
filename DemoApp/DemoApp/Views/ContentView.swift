4778fc05f8dbb30fc8160c6994718b33eb21c65e
Felipe Espinoza <fespinozacast@gmail.com>
2021-01-21 13:47:19 +0100

    Sort demo app

---
+++ DemoApp/DemoApp/Views/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  DemoApp
+//
+//  Created by Felipe Espinoza on 19/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("DNA")) {
+                    NavigationLink("Fonts", destination: EmptyView())
+                    NavigationLink("Colors", destination: ColorsView())
+                    Text("Spacing")
+                }
+
+                Section(header: Text("Basic Components")) {
+                    Text("Labels")
+                    Text("Buttons")
+                }
+            }.navigationTitle("SATS Core Demo")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
