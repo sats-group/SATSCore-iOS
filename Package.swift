b813d6f752cfb0c088bd0dfc46587eb98373e8fb
Felipe Espinoza <fespinozacast@gmail.com>
2021-02-04 12:46:09 +0100

    Add font demos

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SATSCore",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v12), .tvOS(.v14)],
     products: [
         .library(
             name: "SATSCore",
