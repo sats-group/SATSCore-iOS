8f1b1534013a7ac67894b79d2e85ec9bbbdc6270
Felipe Espinoza <fespinozacast@gmail.com>
2021-02-08 13:43:18 +0100

    Add internal ColorName enum
    to present the string names of colors defined in assets and we avoid
    miss-spelling names of colors defined in the asset catalog, as well, we have
    a test that checks the presence of those colors, as that can only be done
    during runtime.

---
+++ Sources/SATSCore/DNA/Colors/ColorName.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+enum ColorName: String, CaseIterable {
+    case backgroundPrimary
+    case backgroundSecondary
+    case backgroundShimmer
+    case backgroundTabbar
+    case backgroundSurfaceStart
+    case backgroundSurfaceEnd
+
+    case buttonPrimary
+    case buttonPrimaryHighlight
+    case buttonPrimaryDisabled
+
+    case buttonSecondary
+    case buttonSecondaryHighlight
+    case buttonSecondaryDisabled
+
+    case buttonCta
+    case buttonCtaHighlight
+    case buttonCtaDisabled
+
+    case onButtonPrimary
+    case onButtonPrimarySecondary
+
+    case onButtonSecondary
+    case onButtonSecondarySecondary
+
+    case onButtonCta
+    case onButtonCtaSecondary
+
+    case onBackgroundSurface
+    case onBackgroundSurfaceSecondary
+
+    case onBackgroundPrimary
+    case onBackgroundPrimarySecondary
+
+    case waitlist
+
+    case link
+
+    case signalSuccess
+    case signalWarning
+    case signalDelete
+    case signalError
+
+    case border
+}
