f0c7308ff6282c405ba071aa28a8902f7e102afc
Felipe Espinoza <fespinozacast@gmail.com>
2021-02-09 13:09:13 +0100

    Add a way to use SwiftUI's environment to resolve themed colors

--- Sources/SATSCore/DNA/Colors/ColorTheme.swift
+++ Sources/SATSCore/DNA/Colors/ColorTheme.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+/// Collection of colors that define a color theme for the app
 public struct ColorTheme {
     public let name: String
 
@@ -11,6 +12,20 @@ public struct ColorTheme {
 
     public let backgroundTopStart: UIColor
     public let backgroundTopEnd: UIColor
+
+    /// Given an static color name, return the appropriate value from the theme
+    subscript(index: ColorName) -> UIColor? {
+        switch index {
+        case .buttonPrimary: return buttonPrimary
+        case .buttonPrimaryHighlight: return buttonPrimaryHighlight
+        case .buttonPrimaryDisabled: return buttonPrimaryDisabled
+        case .selection: return selection
+        case .backgroundTopStart: return backgroundTopStart
+        case .backgroundTopEnd: return backgroundTopEnd
+        default:
+            return nil
+        }
+    }
 }
 
 public extension ColorTheme {
