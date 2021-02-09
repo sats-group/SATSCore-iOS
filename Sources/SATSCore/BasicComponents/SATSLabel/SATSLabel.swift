f87be52a44d539a14f61a6636ce4c227b37dee67
Felipe Espinoza <fespinozacast@gmail.com>
2021-02-09 14:00:38 +0100

    Add documentation and default color to SATSLabel

--- Sources/SATSCore/BasicComponents/SATSLabel/SATSLabel.swift
+++ Sources/SATSCore/BasicComponents/SATSLabel/SATSLabel.swift
@@ -4,13 +4,19 @@ public class SATSLabel: UILabel {
     let style: SATSFont.TextStyle
     let variant: SATSFont.FontVariation
 
-    public init(style: SATSFont.TextStyle, variant: SATSFont.FontVariation = .default, withAutoLayout: Bool = false) {
+    /// Encapsulation of the base text styling for UIKit
+    /// - Parameters:
+    ///   - style: the semantic style of a label, H1, H2, Large, etc
+    ///   - variant: font variations: regular, emphasis, strong
+    ///   - withAutoLayout: to determine if to use auto layout or not (default `true`)
+    public init(style: SATSFont.TextStyle, variant: SATSFont.FontVariation = .default, withAutoLayout: Bool = true) {
         self.style = style
         self.variant = variant
 
         super.init(frame: .zero)
 
         self.font = SATSFont.font(style: style, variant: variant)
+        self.textColor = .onBackgroundPrimary
         self.translatesAutoresizingMaskIntoConstraints = !withAutoLayout
     }
 
