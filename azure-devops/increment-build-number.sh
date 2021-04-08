6461ad8ef5face8b7d4d774094ae938426501ca2
Felipe Espinoza <fespinozacast@gmail.com>
2021-04-08 09:42:49 +0200

    Update reference to plist

--- azure-devops/increment-build-number.sh
+++ azure-devops/increment-build-number.sh
@@ -1,2 +1,2 @@
 buildNumber=$1
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" Sats-MemberApp/Resources/Plists/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" DemoApp/DemoApp/App/Info.plist
