2d6a161a1b41dd07debcf6a3918204e0e76226d1
Felipe Espinoza <fespinozacast@gmail.com>
2021-04-23 11:27:21 +0200

    Add vertical separator utility method

--- Sources/SATSCore/Extensions/UIKit/UIView-Utilities.swift
+++ Sources/SATSCore/Extensions/UIKit/UIView-Utilities.swift
@@ -16,4 +16,20 @@ public extension UIView {
 
         return separatorView
     }
+
+    /// Makes a 1pt line view that you can use as separator
+    /// the position and width of this separator must be handled with the view that calls this method
+    func verticalSeparatorView() -> UIView {
+        let separatorView = UIView(withAutoLayout: true)
+        separatorView.backgroundColor = .border
+
+        NSLayoutConstraint.activate([
+            separatorView.widthAnchor.constraint(equalToConstant: 1),
+            // we set a minimum width as to avoid invoking this method and not seeing it
+            // as the view by default would have width equal to 0
+            separatorView.heightAnchor.constraint(greaterThanOrEqualToConstant: 20),
+        ])
+
+        return separatorView
+    }
 }
