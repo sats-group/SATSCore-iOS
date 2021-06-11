909f3c887b3b9b7f45d205ddcb3117515c43d6cd
Felipe Espinoza <fespinozacast@gmail.com>
2021-06-11 14:52:38 +0200

    Add `ScrollReaderView` component
    A view that helps to "bubble up" the scroll position for a SwiftUI content.

---
+++ DemoApp/DemoApp/Views/Components/ScrollReaderView/ScrollReaderDemoView.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+
+struct ScrollReaderDemoView: View {
+    @State var scrollValue: CGFloat = 0
+
+    var body: some View {
+        ScrollReaderView(scrollValue: $scrollValue) {
+            Rectangle()
+                .foregroundColor(.shimmer)
+                .overlay(
+                    VStack {
+                        Text("Top").bold()
+                        Spacer()
+                        Text("Very long content").bold()
+                        Spacer()
+                        Text("Bottom").bold()
+                        Spacer().frame(height: 100)
+                    }
+                )
+                .frame(height: 1200)
+        }
+        .overlay(
+            VStack {
+                Spacer()
+
+                Text("\(scrollValue)")
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue.opacity(0.2))
+            }
+        )
+        .navigationTitle("ScrollReaderDemoView")
+    }
+}
+
+struct ScrollReaderDemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ScrollReaderDemoView()
+        }
+    }
+}
