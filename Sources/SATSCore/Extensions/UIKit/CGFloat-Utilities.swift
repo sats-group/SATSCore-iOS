e3685ba2a3c7b2612be23efd3d312b556885f478
Felipe Espinoza <fespinozacast@gmail.com>
2021-06-11 14:47:47 +0200

    Add `CGFloat` `readableWidthM` and `readableWidthL` constants
    To use them in a SwiftUI context in order to have a maxWidth for the readable
    area

---
+++ Sources/SATSCore/Extensions/UIKit/CGFloat-Utilities.swift
@@ -0,0 +1,11 @@
+import CoreGraphics
+
+public extension CGFloat {
+    /// Convenience constant for maxWidth in SwiftUI. Smaller than
+    /// UIKit readableContent's width
+    static let readableWidthM: CGFloat = 540
+
+    /// Convenience constant for maxWidth in SwiftUI. Replicates UIKit's
+    /// readableContent guide's width
+    static let readableWidthL: CGFloat = 664
+}
