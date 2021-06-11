3f350474ceb90ba3ca8d93fb0d59cccf8cf8c76e
Felipe Espinoza <fespinozacast@gmail.com>
2021-06-11 14:48:55 +0200

    Add convenience `SimpleRepresentable<View: UIView>`
    To adapt "in-line" UIKit views to be used in SwiftUI.
    
    Example:
    
    ```swift
        SimpleRepresentable<ErrorView> { errorView in
            errorView.onRetry = viewModel.fetchData
            errorView.configure(with: errorViewData)
        }
    ```

---
+++ Sources/SATSCore/Extensions/SwiftUI/Representable/SimpleRepresentable.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+/** A simple wrapper for UIViews for SwiftUI
+
+ This simple wrapper allows to use a UIKit view within SwiftUI without having to
+ write a custom adapter.
+
+ `onCreation` will be called on `makeUIView` so the caller can specify
+    custom, type-safe options for the view.
+
+ Example:
+
+    ```
+    SimpleRepresentable<ErrorView> { errorView in
+        errorView.onRetry = viewModel.fetchData
+        errorView.configure(with: errorViewData)
+    }
+    ```
+*/
+@available(iOS 13.0, *) public struct SimpleRepresentable<View: UIView>: UIViewRepresentable {
+    let onCreation: (View) -> Void
+
+    public init(onCreation: @escaping (View) -> Void) {
+        self.onCreation = onCreation
+    }
+
+    public func makeUIView(context: Context) -> View {
+        let view = View()
+        onCreation(view)
+        return view
+    }
+
+    public func updateUIView(_ uiView: View, context: Context) {}
+}
