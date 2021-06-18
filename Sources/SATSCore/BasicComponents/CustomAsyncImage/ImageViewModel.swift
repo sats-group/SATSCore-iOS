36785d60156be72bb4c91c83f20c4d02869fb484
Felipe Espinoza <fespinozacast@gmail.com>
2021-06-18 14:52:14 +0200

    Add internal `ImageViewModel`

---
+++ Sources/SATSCore/BasicComponents/CustomAsyncImage/ImageViewModel.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+/// Implementation detail for `CustomAsyncImage`
+class ImageViewModel: ObservableObject {
+    @Published var state: ImageViewData = .empty
+
+    private var imageClient: ImageClient { Config.imageClient }
+
+    init(state: ImageViewData) {
+        self.state = state
+    }
+
+    func loadImage() {
+        guard case let .remote(imageUrl) = state else { return }
+
+        state = .loading
+        imageClient.loadImage(with: imageUrl) { [weak self] result in
+            switch result {
+            case let .success(image):
+                self?.state = .image(image)
+            case .failure:
+                self?.state = .empty
+            }
+        }
+    }
+}
