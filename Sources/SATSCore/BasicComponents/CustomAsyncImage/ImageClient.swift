6665986d9113a142b2e23472ccaddfca7b3a5d41
Felipe Espinoza <fespinozacast@gmail.com>
2021-06-18 14:50:23 +0200

    Add internal `DefaultImageClient` implementation

--- Sources/SATSCore/BasicComponents/CustomAsyncImage/ImageClient.swift
+++ Sources/SATSCore/BasicComponents/CustomAsyncImage/ImageClient.swift
@@ -13,3 +13,39 @@ public protocol ImageClient {
     ///                   value if the operation was a success, an error otherwise
     func loadImage(with url: URL, onCompletion: @escaping (Result<Image, Error>) -> Void)
 }
+
+/// Internal only type as a simple/default implementation of the `ImageClient` protocol.
+class DefaultImageClient: ImageClient {
+    var task: URLSessionDataTask?
+
+    func loadImage(with url: URL, onCompletion: @escaping (Result<Image, Error>) -> Void) {
+        task = URLSession.shared.dataTask(with: url) { imageData, _, error in
+            if let imageData = imageData {
+                if let uiImage = UIImage(data: imageData) {
+                    let image = Image(uiImage: uiImage)
+                    onCompletion(.success(image))
+                } else {
+                    onCompletion(.failure(ImageError.decodeError))
+                }
+            } else if let error = error {
+                onCompletion(.failure(error))
+            } else {
+                onCompletion(.failure(ImageError.noData))
+            }
+        }
+
+        task?.resume()
+    }
+
+    enum ImageError: LocalizedError {
+        case decodeError
+        case noData
+
+        var errorDescription: String? {
+            switch self {
+            case .decodeError: return "We got the image data but coudn't convert it to a proper image"
+            case .noData: return "We didn't receive any data, but no error"
+            }
+        }
+    }
+}
