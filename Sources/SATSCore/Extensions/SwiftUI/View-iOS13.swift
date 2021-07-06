c864dc1893d0310495ee0aa8381b851d28d9fe26
Felipe Espinoza <fespinozacast@gmail.com>
2021-07-06 11:13:04 +0200

    Fix typo

--- Sources/SATSCore/Extensions/SwiftUI/View-iOS13.swift
+++ Sources/SATSCore/Extensions/SwiftUI/View-iOS13.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 extension View {
-    /// A easy way to ignore safe area compatible with iOS 13+
+    /// An easy way to ignore safe area compatible with iOS 13+
     @available(iOS, deprecated: 14, message: "Now you should just use `ignoreSafeArea()`")
     public func compatibleIgnoreSafeArea() -> some View {
         Group {
