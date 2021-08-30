dcafe7a20eebfac1ad427e46b1141e63279c2598
Felipe Espinoza <fespinozacast@gmail.com>
2021-08-30 12:37:31 +0200

    Add proposal for actionSheetViewData

---
+++ Sources/SATSCore/Extensions/SwiftUI/ViewData/ActionSheetViewData.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+public struct ActionSheetViewData: Identifiable, Equatable {
+    public let id: String
+    public let title: String
+    public let message: String?
+    public let actions: [ActionViewData]
+
+    public struct ActionViewData: Equatable {
+        public let title: String
+        public let perform: () -> Void
+
+        public static func == (lhs: Self, rhs: Self) -> Bool {
+            lhs.title == rhs.title
+        }
+    }
+}
+
+public extension ActionSheet {
+    init(viewData: ActionSheetViewData) {
+        var buttons: [Button] = viewData.actions
+            .map { action in
+                Button.default(Text(action.title), action: action.perform)
+            }
+        buttons.append(.cancel())
+
+        self.init(
+            title: Text(viewData.title),
+            message: viewData.message.map { Text($0) },
+            buttons: buttons
+        )
+    }
+}
