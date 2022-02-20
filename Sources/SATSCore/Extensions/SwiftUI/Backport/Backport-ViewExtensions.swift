adf5b40f023e7ed4608617894baf3403ddb36101
Felipe Espinoza <fespinozacast@gmail.com>
2022-02-20 21:31:47 +0100

    Backport the `.searchable` modifier

--- Sources/SATSCore/Extensions/SwiftUI/Backport/Backport-ViewExtensions.swift
+++ Sources/SATSCore/Extensions/SwiftUI/Backport/Backport-ViewExtensions.swift
@@ -6,6 +6,18 @@ public extension View {
 
 public extension Backport where Content: View {
     @available(iOS, deprecated: 15, message: "This is a backported version that is not neeeded anymore")
+    @ViewBuilder func searchable(
+        text: Binding<String>,
+        prompt: Text? = nil
+    ) -> some View {
+        if #available(iOS 15, *) {
+            content.searchable(text: text, placement: .navigationBarDrawer(displayMode: .always), prompt: prompt)
+        } else {
+            content
+        }
+    }
+
+    @available(iOS, deprecated: 15, message: "This is a backported version that is not neeeded anymore")
     @ViewBuilder func tint(_ color: Color?) -> some View {
         if #available(iOS 15, *) {
             content.tint(color)
