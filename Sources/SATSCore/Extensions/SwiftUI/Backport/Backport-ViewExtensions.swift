5f62749731d4e8761d7790722e2a135a183b64a3
Felipe Espinoza <fespinozacast@gmail.com>
2022-02-20 21:31:29 +0100

    Add backport for the `.tint` modifier

--- Sources/SATSCore/Extensions/SwiftUI/Backport/Backport-ViewExtensions.swift
+++ Sources/SATSCore/Extensions/SwiftUI/Backport/Backport-ViewExtensions.swift
@@ -4,3 +4,14 @@ public extension View {
     var backport: Backport<Self> { Backport(self) }
 }
 
+public extension Backport where Content: View {
+    @available(iOS, deprecated: 15, message: "This is a backported version that is not neeeded anymore")
+    @ViewBuilder func tint(_ color: Color?) -> some View {
+        if #available(iOS 15, *) {
+            content.tint(color)
+        } else {
+            // deprecated version
+            content.accentColor(color)
+        }
+    }
+}
