453e414d3d59b6270df1bbda90e1432959d2048c
PetterSW <petter.wibstad@icloud.com>
2022-03-29 09:21:01 +0200

    spacing

--- Sources/SATSCore/DNA/Spacings/Spacings.swift
+++ Sources/SATSCore/DNA/Spacings/Spacings.swift
@@ -19,6 +19,7 @@ public extension CGFloat {
     static let spacingXXXL: CGFloat = 128
 
     // MARK: Corner Radius
+
     /// radius of 8 pts
     static let cornerRadiusS: CGFloat = 8
     /// radius of 12 pts
