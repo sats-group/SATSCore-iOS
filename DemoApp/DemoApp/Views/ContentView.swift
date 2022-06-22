32dba5f65f550c942404c718cbd1d317546766a4
Andrew Nielsen <andrew.nielsen@sats.no>
2022-06-22 16:59:35 +0200

    Replace RadioButton with RadioGroup.
    Since radio buttons deal with collections it makes sense to have a RadioGroup component which accepts the options and a binding for selection.
    This can later be extended to support its own styles if necessary.

--- DemoApp/DemoApp/Views/ContentView.swift
+++ DemoApp/DemoApp/Views/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
                         NavigationLink("ScrollReaderView", destination: ScrollReaderDemoView())
                         NavigationLink("TabSelectionView", destination: TabSelectionDemoView())
                         NavigationLink("TopBar", destination: TopBarDemoView())
-                        NavigationLink("RadioButton", destination: RadioButtonView())
+                        NavigationLink("RadioGroup", destination: RadioGroupView())
                     }
                 }
 
---
+++ Sources/SATSCore/Components/RadioGroup/RadioGroup.swift
@@ -0,0 +1,88 @@
+import SwiftUI
+
+public struct RadioGroup<Option: Identifiable, Label: View>: View {
+    @Binding private var selected: Option
+    private let options: [Option]
+    private let label: (Option) -> Label
+
+    public init(
+        selected: Binding<Option>,
+        options: [Option],
+        label: @escaping (Option) -> Label
+    ) {
+        _selected = selected
+        self.options = options
+        self.label = label
+    }
+
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            ForEach(options) { option in
+                HStack(spacing: .spacingM) {
+                    RadioButton(isSelected: option.id == selected.id)
+                    label(option)
+                }
+                .padding(EdgeInsets(vertical: .spacingM, horizontal: 0))
+                .onTapGesture { selected = option }
+            }
+        }
+    }
+}
+
+extension RadioGroup {
+    struct RadioButton: View {
+        let isSelected: Bool
+
+        private var fill: Color { isSelected ? .onBackgroundEnabledOn : .graphicalElements1 }
+        private var lineWidth: CGFloat = 2
+
+        init(isSelected: Bool) {
+            self.isSelected = isSelected
+        }
+
+        var body: some View {
+            Circle()
+                .strokeBorder(fill, lineWidth: lineWidth)
+                .frame(size: 24)
+                .if(isSelected, transform: { circle in
+                    circle
+                        .overlay(
+                            Circle()
+                                .fill(fill)
+                                .padding(lineWidth * 2)
+                        )
+                })
+        }
+    }
+}
+
+struct RadioGroup_Previews: PreviewProvider {
+    enum PreviewOption: String, Identifiable, Hashable, CaseIterable {
+        case gxClass = "GX class"
+        case ptSession = "PT session"
+        case studioTraining = "Studio Training"
+
+        var id: Int { hashValue }
+    }
+
+    struct Demo: View {
+        @State private var selected: PreviewOption = .gxClass
+
+        var body: some View {
+            RadioGroup(
+                selected: $selected,
+                options: PreviewOption.allCases,
+                label: { Text($0.rawValue) }
+            )
+            .padding()
+            .previewLayout(.sizeThatFits)
+        }
+    }
+
+    static var previews: some View {
+        Demo()
+
+        Demo()
+            .preferredColorScheme(.dark)
+    }
+}
