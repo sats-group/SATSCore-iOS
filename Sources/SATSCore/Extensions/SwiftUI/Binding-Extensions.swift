3b836ae217b3e79612f67b8ec525b70a8aaa0b4c
Felipe Espinoza <fespinozacast@gmail.com>
2022-12-15 10:20:16 +0100

    Add `Binding.map` extension

---
+++ Sources/SATSCore/Extensions/SwiftUI/Binding-Extensions.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension Binding {
+    /// A way to create a new binding from another, as long as we can transform the values both ways
+    /// - Parameters:
+    ///   - from: function that maps the binding `Value` to a `NewType`
+    ///   - to: function that maps a value of `NewType` to the original binding `Value`
+    /// - Returns: a binding that is a transformed interface for an original binding
+    func map<NewType>(from: @escaping (Value) -> NewType, to: @escaping (NewType) -> Value) -> Binding<NewType> {
+        .init(
+            get: { from(wrappedValue) },
+            set: { newValue in wrappedValue = to(newValue) }
+        )
+    }
+}
