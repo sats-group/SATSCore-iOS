d8bd54de1c1e9e87384f3a4803ecaf883f31419c
Felipe Espinoza <fespinozacast@gmail.com>
2023-03-02 09:46:56 +0100

    Add `alignment` parameter to `View.frame(size:)`

--- Sources/SATSCore/Extensions/SwiftUI/View-Utilities.swift
+++ Sources/SATSCore/Extensions/SwiftUI/View-Utilities.swift
@@ -3,7 +3,8 @@ import SwiftUI
 extension View {
     /// A convenience to the `frame` method to define same width/height dimensions
     /// - Parameter size: the value for both width and height
-    public func frame(size: CGFloat) -> some View {
-        frame(width: size, height: size)
+    /// - Parameter alignment: alignment for the frame `.center` as default
+    public func frame(size: CGFloat, alignment: Alignment = .center) -> some View {
+        frame(width: size, height: size, alignment: alignment)
     }
 }
