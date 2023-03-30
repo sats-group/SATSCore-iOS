1b1ff36d7ea826f6817c8d3fdfe2db2ba3872891
PetterSW <petter.wibstad@icloud.com>
2023-03-30 09:58:27 +0200

    Remove Assets

--- DemoApp/DemoApp/Utils/DemoAssetName/DemoAssetName.swift
+++ DemoApp/DemoApp/Utils/DemoAssetName/DemoAssetName.swift
@@ -2,21 +2,6 @@ import SwiftUI
 
 enum DemoAssetName: String, CaseIterable {
     case defaultProfile = "default-profile"
-    case moreOptionsButton
-
-    // Tab Bar
-    case tabHomeOn = "home-on"
-    case tabHomeOff = "home-off"
-
-    case tabBookingOn = "book-on"
-    case tabBookingOff = "book-off"
-
-    case tabTrainingOn = "training-on"
-    case tabTrainingOff = "training-off"
-
-    case tabProfileOn = "profile-on"
-    case tabProfileOff = "profile-off"
-
     case checkmark
 }
 
