b19dbbbe40d6c601e280dc7ffa7985d4d9557cb9
PetterSW <petter.wibstad@icloud.com>
2023-09-28 12:09:32 +0200

    Make SATSRewardsLabel a component without referencing to SATSLabel

---
+++ DemoApp/DemoApp/Views/BasicComponents/RewardsTagDemoView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct RewardsTagDemoView: View {
+    var body: some View {
+        VStack(spacing: .spacingM) {
+            RewardsTag(level: .blue)
+            RewardsTag(level: .silver)
+            RewardsTag(level: .gold)
+            RewardsTag(level: .platinum)
+        }
+        .padding(.spacingM)
+        .navigationTitle("Rewards Tag")
+    }
+}
+
+#Preview {
+    RewardsTagDemoView()
+}
---
+++ Sources/SATSCore/BasicComponents/SATSLabel/RewardsTag.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+public struct RewardsTag: View {
+    let level: Level
+
+    public init(level: Level) {
+        self.level = level
+    }
+
+    public var body: some View {
+        Text(level.rawValue)
+            .foregroundStyle(Color.onRewards)
+            .textCase(.uppercase)
+            .satsFont(.small)
+            .padding(.vertical, .spacingXXS)
+            .padding(.horizontal, .spacingS)
+            .background(level.background)
+            .cornerRadius(4)
+    }
+}
+
+#Preview {
+    VStack(spacing: .spacingM) {
+        RewardsTag(level: .blue)
+        RewardsTag(level: .silver)
+        RewardsTag(level: .gold)
+        RewardsTag(level: .platinum)
+    }
+}
+
+public extension RewardsTag {
+    enum Level: String {
+        case blue = "Blue"
+        case silver = "Silver"
+        case gold = "Gold"
+        case platinum = "Platinum"
+
+        var background: Color {
+            switch self {
+            case .blue: .rewardsBlue
+            case .silver: .rewardsSilver
+            case .gold: .rewardsGold
+            case .platinum: .rewardsPlatinum
+            }
+        }
+    }
+}
