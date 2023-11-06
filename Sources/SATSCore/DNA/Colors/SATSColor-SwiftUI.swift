919ceceb80cfaeab1c3283146484a59a3557dc99
Felipe Espinoza <fespinozacast@gmail.com>
2023-11-06 10:50:04 +0100

    WIP

---
+++ Sources/SATSCore/DNA/Colors/SATSColor-SwiftUI.swift
@@ -0,0 +1,250 @@
+import SwiftUI
+
+struct NeoSATSColorSwiftUI {
+    // MARK: - Buttons
+
+    let primaryButton: Color = Self.satsColor(for: .primaryButton)
+    let primaryButtonHighlight: Color = Self.satsColor(for: .primaryButtonHighlight)
+    let primaryButtonDisabled: Color = Self.satsColor(for: .primaryButtonDisabled)
+
+    let secondaryButton: Color = Self.satsColor(for: .secondaryButton)
+    let secondaryButtonHighlight: Color = Self.satsColor(for: .secondaryButtonHighlight)
+    let secondaryButtonDisabled: Color = Self.satsColor(for: .secondaryButtonDisabled)
+
+    let cleanButton: Color = Self.satsColor(for: .cleanButton)
+    let cleanButtonHighlight: Color = Self.satsColor(for: .cleanButtonHighlight)
+    let cleanButtonDisabled: Color = Self.satsColor(for: .cleanButtonDisabled)
+
+    let cleanSecondaryButton: Color = Self.satsColor(for: .cleanSecondaryButton)
+    let cleanSecondaryButtonHighlight: Color = Self.satsColor(for: .cleanSecondaryButtonHighlight)
+    let cleanSecondaryButtonDisabled: Color = Self.satsColor(for: .cleanSecondaryButtonDisabled)
+
+    let ctaButton: Color = Self.satsColor(for: .ctaButton)
+    let ctaButtonHighlight: Color = Self.satsColor(for: .ctaButtonHighlight)
+    let ctaButtonDisabled: Color = Self.satsColor(for: .ctaButtonDisabled)
+
+    let action: Color = Self.satsColor(for: .action)
+    let actionHighlight: Color = Self.satsColor(for: .actionHighlight)
+    let actionDisabled: Color = Self.satsColor(for: .actionDisabled)
+
+    let waitingListButton: Color = Self.satsColor(for: .waitingListButton)
+    let waitingListButtonHighlight: Color = Self.satsColor(for: .waitingListButtonHighlight)
+    let waitingListButtonDisabled: Color = Self.satsColor(for: .waitingListButtonDisabled)
+
+    let deleteButton: Color = Self.satsColor(for: .deleteButton)
+    let deleteButtonHighlight: Color = Self.satsColor(for: .deleteButtonHighlight)
+    let deleteButtonDisabled: Color = Self.satsColor(for: .deleteButtonDisabled)
+
+    // MARK: - Graphical Elements
+
+    // MARK: Divider
+
+    let divider: Color = Self.satsColor(for: .divider)
+
+    // MARK: Progress Bar
+
+    let progressBarIndicator: Color = Self.satsColor(for: .progressBarIndicator)
+    let progressBarBackground: Color = Self.satsColor(for: .progressBarBackground)
+
+    // MARK: Graphs
+
+    let graphBarPrimary: Color = Self.satsColor(for: .graphBarPrimary)
+    let graphBarSecondary: Color = Self.satsColor(for: .graphBarSecondary)
+    let graphBackgroundPrimary: Color = Self.satsColor(for: .graphBackgroundPrimary)
+    let graphBackgroundSecondary: Color = Self.satsColor(for: .graphBackgroundSecondary)
+    let graphUpwardsTrend: Color = Self.satsColor(for: .graphUpwardsTrend)
+    let graphNeutralTrend: Color = Self.satsColor(for: .graphNeutralTrend)
+    let graphDownwardsTrend: Color = Self.satsColor(for: .graphDownwardsTrend)
+
+    // MARK: Selector
+
+    let selectorUnselected: Color = Self.satsColor(for: .selectorUnselected)
+    let selectorUnselectedDisabled: Color = Self.satsColor(for: .selectorUnselectedDisabled)
+
+    let selectorSelected: Color = Self.satsColor(for: .selectorSelected)
+    let selectorSelectedDisabled: Color = Self.satsColor(for: .selectorSelectedDisabled)
+
+    // MARK: Toggle
+
+    let toggleUnselected: Color = Self.satsColor(for: .toggleUnselected)
+    let toggleUnselectedDisabled: Color = Self.satsColor(for: .toggleUnselectedDisabled)
+    let toggleInteractiveElement: Color = Self.satsColor(for: .toggleInteractiveElement)
+    let toggleSelected: Color = Self.satsColor(for: .toggleSelected)
+    let toggleSelectedDisabled: Color = Self.satsColor(for: .toggleSelectedDisabled)
+
+    // MARK: Icons
+
+    let iconPrimary: Color = Self.satsColor(for: .iconPrimary)
+    let iconSecondary: Color = Self.satsColor(for: .iconSecondary)
+    let iconFixed: Color = Self.satsColor(for: .iconFixed)
+    let iconPositive: Color = Self.satsColor(for: .iconPositive)
+    let iconAttention: Color = Self.satsColor(for: .iconAttention)
+    let iconNegative: Color = Self.satsColor(for: .iconNegative)
+    let iconWaitingList: Color = Self.satsColor(for: .iconWaitingList)
+    let iconDelete: Color = Self.satsColor(for: .iconDelete)
+
+    // MARK: Indicators
+
+    let indicatorPositive: Color = Self.satsColor(for: .indicatorPositive)
+    let indicatorPositiveAlternate: Color = Self.satsColor(for: .indicatorPositiveAlternate)
+
+    let indicatorAttention: Color = Self.satsColor(for: .indicatorAttention)
+    let indicatorAttentionAlternate: Color = Self.satsColor(for: .indicatorAttentionAlternate)
+
+    let indicatorNegative: Color = Self.satsColor(for: .indicatorNegative)
+    let indicatorNegativeAlternate: Color = Self.satsColor(for: .indicatorNegativeAlternate)
+
+    let indicatorNeutralList: Color = Self.satsColor(for: .indicatorNeutralList)
+    let indicatorNeutralListAlternate: Color = Self.satsColor(for: .indicatorNeutralListAlternate)
+
+    // MARK: Signal
+
+    let signalSuccess: Color = Self.satsColor(for: .signalSuccess)
+    let signalWarning: Color = Self.satsColor(for: .signalWarning)
+    let signalError: Color = Self.satsColor(for: .signalError)
+    let signalNeutral: Color = Self.satsColor(for: .signalNeutral)
+    let signalWaitingList: Color = Self.satsColor(for: .signalWaitingList)
+
+    // MARK: Tags
+
+    let tagPrimary: Color = Self.satsColor(for: .tagPrimary)
+    let tagSecondary: Color = Self.satsColor(for: .tagSecondary)
+    let tagFeatured: Color = Self.satsColor(for: .tagFeatured)
+
+    // MARK: Rewards
+
+    let rewardsBlue: Color = Self.satsColor(for: .rewardsBlue)
+    let rewardsSilver: Color = Self.satsColor(for: .rewardsSilver)
+    let rewardsGold: Color = Self.satsColor(for: .rewardsGold)
+    let rewardsPlatinum: Color = Self.satsColor(for: .rewardsPlatinum)
+
+    // MARK: Workouts
+
+    let workoutsPt: Color = Self.satsColor(for: .workoutsPt)
+    let workoutsGx: Color = Self.satsColor(for: .workoutsGx)
+    let workoutsTreatments: Color = Self.satsColor(for: .workoutsTreatments)
+    let workoutsGymfloor: Color = Self.satsColor(for: .workoutsGymfloor)
+    let workoutsOwnTraining: Color = Self.satsColor(for: .workoutsOwnTraining)
+
+    // MARK: - Backgrounds and Surfaces
+
+    // MARK: Background
+
+    let backgroundPrimary: Color = Self.satsColor(for: .backgroundPrimary)
+    let backgroundSecondary: Color = Self.satsColor(for: .backgroundSecondary)
+    let backgroundFixed: Color = Self.satsColor(for: .backgroundFixed)
+
+    // MARK: Surface
+
+    let surfacePrimary: Color = Self.satsColor(for: .surfacePrimary)
+    let surfaceSecondary: Color = Self.satsColor(for: .surfaceSecondary)
+    let surfaceFixed: Color = Self.satsColor(for: .surfaceFixed)
+
+    // MARK: Signal Surface
+
+    let signalSurfaceSuccess: Color = Self.satsColor(for: .signalSurfaceSuccess)
+    let signalSurfaceWarning: Color = Self.satsColor(for: .signalSurfaceWarning)
+    let signalSurfaceError: Color = Self.satsColor(for: .signalSurfaceError)
+    let signalSurfaceWaitlist: Color = Self.satsColor(for: .signalSurfaceWaitlist)
+    let signalSurfaceNeutral: Color = Self.satsColor(for: .signalSurfaceNeutral)
+
+    // MARK: - "On" Colors
+
+    // MARK: On Buttons
+
+    let onPrimaryButton: Color = Self.satsColor(for: .onPrimaryButton)
+    let onPrimaryButtonDisabled: Color = Self.satsColor(for: .onPrimaryButtonDisabled)
+
+    let onSecondaryButton: Color = Self.satsColor(for: .onSecondaryButton)
+    let onSecondaryButtonDisabled: Color = Self.satsColor(for: .onSecondaryButtonDisabled)
+
+    let onCleanButton: Color = Self.satsColor(for: .onCleanButton)
+    let onCleanButtonDisabled: Color = Self.satsColor(for: .onCleanButtonDisabled)
+
+    let onCleanSecondaryButton: Color = Self.satsColor(for: .onCleanSecondaryButton)
+    let onCleanSecondaryButtonDisabled: Color = Self.satsColor(for: .onCleanSecondaryButtonDisabled)
+
+    let onCtaButton: Color = Self.satsColor(for: .onCtaButton)
+    let onCtaButtonDisabled: Color = Self.satsColor(for: .onCtaButtonDisabled)
+
+    let onWaitlistButton: Color = Self.satsColor(for: .onWaitlistButton)
+    let onWaitlistButtonDisabled: Color = Self.satsColor(for: .onWaitlistButtonDisabled)
+
+    let onDeleteButton: Color = Self.satsColor(for: .onDeleteButton)
+    let onDeleteButtonDisabled: Color = Self.satsColor(for: .onDeleteButtonDisabled)
+
+    // MARK: On Tags
+
+    let onTagPrimary: Color = Self.satsColor(for: .onTagPrimary)
+    let onTagSecondary: Color = Self.satsColor(for: .onTagSecondary)
+    let onTagFeatured: Color = Self.satsColor(for: .onTagFeatured)
+
+    // MARK: On Rewards
+
+    let onReward: Color = Self.satsColor(for: .onReward)
+
+    // MARK: On Background
+
+    let onBackgroundPrimary: Color = Self.satsColor(for: .onBackgroundPrimary)
+    let onBackgroundPrimaryAlternate: Color = Self.satsColor(for: .onBackgroundPrimaryAlternate)
+    let onBackgroundPrimaryDisabled: Color = Self.satsColor(for: .onBackgroundPrimaryDisabled)
+
+    let onBackgroundSecondary: Color = Self.satsColor(for: .onBackgroundSecondary)
+    let onBackgroundSecondaryAlternate: Color = Self.satsColor(for: .onBackgroundSecondaryAlternate)
+    let onBackgroundSecondaryDisabled: Color = Self.satsColor(for: .onBackgroundSecondaryDisabled)
+
+    let onBackgroundTertiary: Color = Self.satsColor(for: .onBackgroundTertiary)
+    let onBackgroundTertiaryAlternate: Color = Self.satsColor(for: .onBackgroundTertiaryAlternate)
+    let onBackgroundTertiaryDisabled: Color = Self.satsColor(for: .onBackgroundTertiaryDisabled)
+
+    // MARK: On Surface
+
+    let onSurfacePrimary: Color = Self.satsColor(for: .onSurfacePrimary)
+    let onSurfacePrimaryAlternate: Color = Self.satsColor(for: .onSurfacePrimaryAlternate)
+    let onSurfacePrimaryDisabled: Color = Self.satsColor(for: .onSurfacePrimaryDisabled)
+
+    let onSurfaceSecondary: Color = Self.satsColor(for: .onSurfaceSecondary)
+    let onSurfaceSecondaryAlternate: Color = Self.satsColor(for: .onSurfaceSecondaryAlternate)
+    let onSurfaceSecondaryDisabled: Color = Self.satsColor(for: .onSurfaceSecondaryDisabled)
+
+    let onSurfaceFixed: Color = Self.satsColor(for: .onSurfaceFixed)
+    let onSurfaceFixedAlternate: Color = Self.satsColor(for: .onSurfaceFixedAlternate)
+    let onSurfaceFixedDisabled: Color = Self.satsColor(for: .onSurfaceFixedDisabled)
+
+    let onSurfaceSuccess: Color = Self.satsColor(for: .onSurfaceSuccess)
+    let onSurfaceWarning: Color = Self.satsColor(for: .onSurfaceWarning)
+    let onSurfaceError: Color = Self.satsColor(for: .onSurfaceError)
+    let onSurfaceWaitlist: Color = Self.satsColor(for: .onSurfaceWaitlist)
+    let onSurfaceNeutral: Color = Self.satsColor(for: .onSurfaceNeutral)
+
+    // MARK: On Signal Surface
+
+    let onSignalSurfaceSuccess: Color = Self.satsColor(for: .onSignalSurfaceSuccess)
+    let onSignalSurfaceSuccessAlternate: Color = Self.satsColor(for: .onSignalSurfaceSuccessAlternate)
+
+    let onSignalSurfaceWarning: Color = Self.satsColor(for: .onSignalSurfaceWarning)
+    let onSignalSurfaceWarningAlternate: Color = Self.satsColor(for: .onSignalSurfaceWarningAlternate)
+
+    let onSignalSurfaceError: Color = Self.satsColor(for: .onSignalSurfaceError)
+    let onSignalSurfaceErrorAlternate: Color = Self.satsColor(for: .onSignalSurfaceErrorAlternate)
+
+    let onSignalSurfaceWaitlist: Color = Self.satsColor(for: .onSignalSurfaceWaitlist)
+    let onSignalSurfaceWaitlistAlternate: Color = Self.satsColor(for: .onSignalSurfaceWaitlistAlternate)
+
+    let onSignalSurfaceNeutral: Color = Self.satsColor(for: .onSignalSurfaceNeutral)
+    let onSignalSurfaceNeutralAlternate: Color = Self.satsColor(for: .onSignalSurfaceNeutralAlternate)
+}
+
+private extension NeoSATSColorSwiftUI {
+    static func satsColor(for colorName: SATSColorName) -> Color {
+        guard let definition = SATSColor.definitions[colorName] else {
+            fatalError("❌ no color definition for \(colorName) found")
+        }
+
+        return definition.color
+    }
+}
+
+extension Color {
+    static let satsOne = NeoSATSColorSwiftUI()
+}
